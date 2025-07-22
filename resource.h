c3a1dfd6a3519cdbd16cb4d311b4514cd9c1310a
Afevis <ShizCalev@users.noreply.github.com>
2025-07-22 12:33:14 -0600

    Version checker

--- resource.h
+++ resource.h
@@ -1,3 +1,28 @@
+#pragma once
+
+// Core name & version
+#define FIX_NAME          "MGSM2Fix"
+#define VERSION_STRING    "3.0.0"
+#define PRIMARY_REPO_URL  "https://github.com/nuggslet/MGSM2Fix"
+#define FALLBACK_REPO_URL "https://github.com/h-i-d-e-o/MGSM2Fix"
+
+#define VERSION_MAJOR     3
+#define VERSION_MINOR     0
+#define VERSION_PATCH     0
+
+// Metadata
+#define COMPANY_NAME      "nuggslet"
+#define PRODUCT_NAME      FIX_NAME
+#define FILE_DESCRIPTION  FIX_NAME " ASI Plugin"
+#define INTERNAL_NAME     FIX_NAME ".asi"
+#define ORIGINAL_FILENAME FIX_NAME ".asi"
+#define PRODUCT_VERSION   VERSION_STRING
+#define FILE_VERSION      VERSION_STRING
+#define LEGAL_COPYRIGHT   "(C) 2025 nuggslet. Licensed under the MIT License."
+#define LEGAL_TRADEMARKS  ""
+#define COMMENTS          ""
+
+
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by MGSM2Fix.rc
