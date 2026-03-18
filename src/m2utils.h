d1adb590571ca859133449d3760c22df56fea621
nuggs <nuggslet@protonmail.com>
2026-03-18 02:56:02 +0000

    `SQObjectType` custom spdlog formatter

--- src/m2utils.h
+++ src/m2utils.h
@@ -30,3 +30,39 @@ public:
 	static bool IsSteamOS();
 	static std::string GetSteamOSVersion();
 };
+
+template<>
+struct fmt::formatter<SQObjectType> {
+	constexpr auto parse(format_parse_context & ctx) {
+		return ctx.begin();
+	}
+
+	template <typename FormatContext>
+	constexpr auto format(const SQObjectType & input, FormatContext & ctx) const -> decltype(ctx.out())
+	{
+		static const std::unordered_map<SQObjectType, std::string> types = {
+			{ OT_NULL,          "OT_NULL"          },
+			{ OT_INTEGER,       "OT_INTEGER"       },
+			{ OT_FLOAT,         "OT_FLOAT"         },
+			{ OT_BOOL,          "OT_BOOL"          },
+			{ OT_STRING,        "OT_STRING"        },
+			{ OT_TABLE,         "OT_TABLE"         },
+			{ OT_ARRAY,         "OT_ARRAY"         },
+			{ OT_USERDATA,      "OT_USERDATA"      },
+			{ OT_CLOSURE,       "OT_CLOSURE"       },
+			{ OT_NATIVECLOSURE, "OT_NATIVECLOSURE" },
+			{ OT_GENERATOR,     "OT_GENERATOR"     },
+			{ OT_USERPOINTER,   "OT_USERPOINTER"   },
+			{ OT_THREAD,        "OT_THREAD"        },
+			{ OT_FUNCPROTO,     "OT_FUNCPROTO"     },
+			{ OT_CLASS,         "OT_CLASS"         },
+			{ OT_INSTANCE,      "OT_INSTANCE"      },
+			{ OT_WEAKREF,       "OT_WEAKREF"       },
+		};
+
+		if (types.contains(input)) {
+			return format_to(ctx.out(), "{}", types.at(input));
+		}
+		return format_to(ctx.out(), "OT_{:08x}", static_cast<unsigned>(input));
+	}
+};
